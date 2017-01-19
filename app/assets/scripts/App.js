b40416a77afb41678082f2cb8c27d1ec4be37fb4
Quentin Fonck <quentin.fonck@internet.lu>
2017-01-19 14:51:20 +0100

    Deleting a not used property

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,6 +3,7 @@ import ProductTileHoverEffect from './modules/ProductTileHoverEffect';
 import StickyHeader from './modules/StickyHeader';
 import ProductServicesDisplay from './modules/ProductServicesDisplay';
 import CatSelection from './modules/CatSelection';
+import SelectOnClick from './modules/SelectOnClick';
 import $ from 'jquery';
 
 $('.aid').each(function(){
@@ -21,7 +22,6 @@ $('.product-services__item').each(function(){
 
 //new SelectOnClick($('.hardware-selection__nav'), 'li:not(.hardware-selection__nav__arrow)');
 new CatSelection({
-  element : $('.hardware-selection'),
   nav : $('.hardware-selection__nav'),
   navChildSelector : 'li:not(.hardware-selection__nav__arrow)',
   content : $('.hardware-selection__content')
