810aaf6e0ff1fb2161101456ee5e94a2707f5196
Quentin Fonck <quentin.fonck@internet.lu>
2017-02-03 17:33:26 +0100

    Fixed gulp usemin build error

--- app/assets/scripts/modules/productTileHoverEffect.js
+++ app/assets/scripts/modules/productTileHoverEffect.js
@@ -4,7 +4,7 @@ var ProductTileHoverEffect = function(){
   $('.product-tile').on('mouseenter', event=>{
     $('.product-tile').not($(event.target).closest('.product-tile')).addClass('product-tile--no-hover');
   });
-  $('.product-tile').on('mouseleave', ()=>{
+  $('.product-tile').on('mouseleave', function(){
     $('.product-tile').removeClass('product-tile--no-hover');
   });
 };
